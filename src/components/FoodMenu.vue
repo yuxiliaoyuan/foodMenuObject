263a88591b46bd97ce6e89894d572f6a1e4f7a8e
wanganbang <Wab@123>
2019-09-16 14:01:25 +0800

    效果实现

--- src/components/FoodMenu.vue
+++ src/components/FoodMenu.vue
@@ -1,46 +1,70 @@
 <template>
   <div id="app" align="center">
     <div id="menu">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <van-list v-model="isLoading" :finished="isFinished" @load="onLoad" style="margin-top: 5px;">
+          <van-row type="flex" justify="space-between" v-for="(item, index) in sites" class="item_class" :key="index">
+            <van-col span="4"><div class="item_index">{{index + 1}}</div></van-col>
+            <van-col span="12" align="left" style="line-height: 45px;">{{item.name}}</van-col>
+            <van-col span="4" style="line-height: 45px;">{{item.likes}}</van-col>
+            <van-col span="4"><van-image class="item_img" :src="item.img"></van-image></van-col>
+          </van-row>
+        </van-list>
+      </van-pull-refresh>
     </div>
     <div id="share">
-      <b>分  享  有  礼  ></b>
+      <b>分 享 有 礼 ></b>
     </div>
-    <div id="company">青岛禹锡智慧科技有限公司  技术支持</div>
+    <div id="company">青岛禹锡智慧科技有限公司 技术支持</div>
   </div>
 </template>
 
 <script>
 import $ from 'jquery'
-import {Button, Icon} from 'vant'
+import {
+  Button, Icon, PullRefresh, List,
+  Cell, Row, Col, Image
+} from 'vant'
 
 export default {
   name: 'App',
   components: {
+    [List.name]: List,
+    [Cell.name]: Cell,
     [Button.name]: Button,
-    [Icon.name]: Icon
+    [Icon.name]: Icon,
+    [PullRefresh.name]: PullRefresh,
+    [Row.name]: Row,
+    [Col.name]: Col,
+    [Image.name]: Image
   },
   data () {
     return {
       message: '本店菜品排行榜',
+      isLoading: false,
+      isFinished: true,
       bgstyle: {
         backgroundSize: $(document).width() + ' ' + $(document).height()
       },
       sites:
           [{
-            imgN: '/static/img/2.jpg',
+            img: '/static/img/recommend.png',
             name: '红烧茄子',
-            cp: '这里是菜品介绍',
-            sl: '9999'
+            description: '这里是菜品介绍',
+            likes: '1000',
+            islike: 'like'
           }, {
-            imgN: '/static/img/3.jpg',
-            name: '红烧茄子',
-            cp: '这里是菜品介绍',
-            sl: '9999'
+            img: '/static/img/unrecommend.png',
+            name: '尖椒鸡蛋',
+            description: '这里是菜品介绍',
+            likes: '1280',
+            islike: 'like-o'
           }, {
-            imgN: '/static/img/4.jpg',
-            name: '红烧茄子',
-            cp: '这里是菜品介绍',
-            sl: '9999'
+            img: '/static/img/unrecommend.png',
+            name: '糖醋排骨',
+            description: '这里是菜品介绍',
+            likes: '9999',
+            islike: 'like'
           }]
 
     }
@@ -54,6 +78,61 @@ export default {
   methods: {
     share () {
       alert('share it')
+    },
+    onLoad () {
+      console.log('Loding ...')
+      this.isFinished = false
+      this.isLoading = false
+      this.sites = [{
+        img: '/static/img/recommend.png',
+        name: '红烧茄子',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like'
+      }, {
+        img: '/static/img/unrecommend.png',
+        name: '韭菜鸡蛋',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like-o'
+      }, {
+        img: '/static/img/unrecommend.png',
+        name: '红烧肉',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like-o'
+      }, {
+        img: '/static/img/unrecommend.png',
+        name: '东坡肘子',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like-o'
+      }, {
+        img: '/static/img/unrecommend.png',
+        name: '蚂蚁上树',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like-o'
+      }, {
+        img: '/static/img/unrecommend.png',
+        name: '地三鲜',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like-o'
+      }, {
+        img: '/static/img/unrecommend.png',
+        name: '尖椒豆皮',
+        description: '这里是菜品介绍',
+        likes: '9999',
+        islike: 'like-o'
+      }]
+    },
+    onRefresh () {
+      console.log('Refresh ...')
+      this.isFinished = false
+      this.isLoading = false
+      this.sites = []
+      this.onLoad()
     }
   }
 }
@@ -104,5 +183,26 @@ export default {
     margin-left: 10%;
     margin-right: 10%;
     font-size: small;
+  }
+  .item_class {
+    width: 90%;
+    height: 45px;
+    margin-top: 2px;
+    background-image: url("/static/img/item_bg.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    -moz-background-size: 100% 100%;
+  }
+  .item_img {
+    height: 30px;
+    width: 35px;
+    margin-top: 5px;
+  }
+  .item_index {
+    background-image: url("/static/img/index_num.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 25px;
+    width: 25px;
   }
 </style>
